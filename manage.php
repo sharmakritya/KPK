5d7752af9611fc5398e1fb46b597db66ce056e8d
sharmakritya <sharmakritya@gmail.com>
2015-11-01 23:23:48 +0530

    Updates

--- manage.php
+++ manage.php
@@ -70,40 +70,38 @@ $row=$result->fetch_assoc();
     ?>
 </table>
 <br>
-<span id="newEvent">New Event</span>
-    <div id="newEventCont">
-        <form method="post" action="manage.php">
-            <table>
-                <tr>
-                    <td><label for="Type">Event Type:</label></td>
-                    <td><select id="Type" name="type" required>
-                            <option value="">Select an Event Type..</option>
-                            <option value="1">BirthDay</option>
-                            <option value="2">Marriage</option>
-                            <option value="3">Break Up</option>
-                            <option value="4">Anniversary</option>
-                        </select></td>
-                </tr>
-                <tr>
-                    <td><label for="Name">Name:</label></td>
-                    <td><input type="text" name="name" id="Name" required/></td>
-                </tr>
-                <tr>
-                    <td><label for="description">Description:</label></td>
-                    <td><textarea name="description" id="description" required></textarea></td>
-                </tr>
-                <tr>
-                    <td><label for="date">Date:</label></td>
-                    <td><input type="date" name="date" id="date" required/></td>
-                </tr>
-            </table>
-            <br>
-            <input type="submit" value="Done"/>
-        </form>
-    </div>
+<div id="newEvent">New Event
+<div id="newEventCont">
+    <form method="post" action="manage.php">
+        <table>
+            <tr>
+                <td><label for="Type">Event Type:</label></td>
+                <td><select id="Type" name="type" required>
+                        <option value="">Select an Event Type..</option>
+                        <option value="1">BirthDay</option>
+                        <option value="2">Marriage</option>
+                        <option value="3">Break Up</option>
+                        <option value="4">Anniversary</option>
+                    </select></td>
+            </tr>
+            <tr>
+                <td><label for="Name">Name:</label></td>
+                <td><input type="text" name="name" id="Name" required/></td>
+            </tr>
+            <tr>
+                <td><label for="description">Description:</label></td>
+                <td><textarea name="description" id="description" required></textarea></td>
+            </tr>
+            <tr>
+                <td><label for="date">Date:</label></td>
+                <td><input type="date" name="date" id="date" required/></td>
+            </tr>
+        </table>
+        <br>
+        <input type="submit" value="Done"/>
+    </form>
 </div>
-<div id="side">
-<a href="index.html" style="text-decoration:none">Logout</a>
+</div>
 </div>
 </body>
 </html>
